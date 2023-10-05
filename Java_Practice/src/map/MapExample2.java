44c678eb7474d6972e6941aa1d57a0478fd52d87
Vidya <vidyac266@gmail.com>
2023-10-05 20:03:43 +0530

    hashMap

---
+++ Java_Practice/src/map/MapExample2.java
@@ -0,0 +1,18 @@
+package map;
+import java.util.*;
+
+public class MapExample2 {
+
+	public static void main(String[] args) {
+		Map<Integer,String> map=new HashMap<Integer,String>();  
+		  map.put(100,"Amit");  
+		  map.put(101,"Vijay");  
+		  map.put(102,"Rahul");  
+		  System.out.println(map);  
+		
+		 
+		
+
+	}
+
+}
